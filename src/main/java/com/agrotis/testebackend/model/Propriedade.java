6ca05c7080b9ac89689298d9f8af707bb02ffd50
mario-sergio <mariosergioadriano@gmail.com>
2023-10-16 00:52:35 -0300

    Criando model propriedade

---
+++ src/main/java/com/agrotis/testebackend/model/Propriedade.java
@@ -0,0 +1,43 @@
+package com.agrotis.testebackend.model;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.domain.Persistable;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "propriedade")
+@Entity
+public class Propriedade implements Persistable<UUID>{
+
+    @Id
+    @GenericGenerator(name = "UUIDGenerator", strategy = "uuid2")
+    @GeneratedValue(generator = "UUIDGenerator")
+    @Column(name = "id", nullable = false, updatable = false)
+    private UUID id;
+
+    @Column(name = "nome")
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+
+    @JsonIgnore
+    @Override
+    public boolean isNew() {
+        return id == null;
+    }
+}
